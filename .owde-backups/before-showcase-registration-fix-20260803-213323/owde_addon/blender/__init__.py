577df6a4d5b381a5676c51f1049b1517643c8711
Daniel-Ashley <68601847+Daniel-Ashley@users.noreply.github.com>
2026-08-03 21:37:51 -0400

    Fix OWDE showcase operator and panel registration

--- .owde-backups/before-showcase-registration-fix-20260803-213323/owde_addon/blender/__init__.py
+++ .owde-backups/before-showcase-registration-fix-20260803-213323/owde_addon/blender/__init__.py
@@ -10,17 +10,10 @@ from .panel import (
 from .properties import OWDE_PG_settings
 
 __all__ = [
-"OWDE_OT_create_primitive_gallery",
+    "OWDE_OT_create_primitive_gallery",
     "OWDE_OT_create_tile",
     "OWDE_OT_frame_primitive_gallery",
     "OWDE_PG_settings",
     "OWDE_PT_primitive_inspector",
     "OWDE_PT_tile_builder",
-    "OWDE_OT_create_showcase",
-    "OWDE_PT_showcase",
 ]
-
-from .showcase import (
-    OWDE_OT_create_showcase,
-    OWDE_PT_showcase,
-)
